86182e8b5d200d9bfdbd810c17eea8b452818398
Gangaromal02 <aromalanilkumar.621821@gmail.com>
2022-07-02 15:41:07 +0530

    seaching employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeaddComponent } from './employeeadd/employeeadd.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeaddComponent
+    EmployeeaddComponent,
+    SearchEmployeeComponent
   ],
   imports: [
     BrowserModule,
